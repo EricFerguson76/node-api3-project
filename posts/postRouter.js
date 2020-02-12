b8fda3e1336371d8f6d06e3128e4214a76e8074a
Eric Ferguson <turtle0888@gmail.com>
2020-02-12 15:36:08 -0600

    Create GET for posts

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,12 +2,36 @@ const express = require('express');
 
 const router = express.Router();
 
+router.use(express.json());
+
+const Posts = require('./postDb');
+
 router.get('/', (req, res) => {
   // do your magic!
+  Posts.get(req.query)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Error retrieving the posts' });
+    });
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  const { id } = req.params;
+
+  Posts.getById(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(400).json({ message: 'Post not found' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Error retrieving the post' });
+    });
 });
 
 router.delete('/:id', (req, res) => {
