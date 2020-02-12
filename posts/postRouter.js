1353fe8b7a8374b52d8c24d26fb536a00f906d32
Eric Ferguson <turtle0888@gmail.com>
2020-02-12 16:06:02 -0600

    Create PUT for posts

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -40,6 +40,20 @@ router.delete('/:id', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // do your magic!
+  const { id } = req.params;
+  const postsData = req.body;
+
+  Posts.update(id, postsData)
+    .then(updatedPost => {
+      if (updatedPost) {
+        res.status(200).json(updatedPost);
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Error updating the post' });
+    });
 });
 
 // custom middleware
